da230f8bb32e1201dc67327a30b070260d567570
LucaKamps <111793@st-maartenscollege.nl>
2025-09-15 06:45:58 +0000

    Opdracht les 2

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-let colorlist = ['gold', 'yellow', 'turquoise', 'red']
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(255);
+  canvas = createCanvas(900,200);
+  background('lavender');
+  canvas.parent();
+  noLoop();
 }
 
 function draw() {
-  noStroke()
-  fill(random(colorlist));
-  ellipse(mouseX, mouseY, 25, 25);
+  noStroke();
+  fill('thistle');
+  translate(50,50);
+  rect(0,0,100,100);
+
+  translate(140,0);
+  rect(0,0,100,100);
+  translate(140,0);
+  rect(0,0,100,100);
+
+  translate(140,0);
+  // BEGIN aanpassing
+  push();
+  stroke('cornsilk');
+  strokeWeight(20);
+  fill('teal');
+  translate(0,40);  
+  rect(0,0,100,100); 
+  // EINDE aanpassing
+
+  translate(140,0);
+  rect(0,0,100,100);
+  pop();
+  translate(140,0);
+  rect(0,0,100,100);  
 }
