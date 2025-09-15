03c87e81a95fa2cc991edcb45457b29ab7f511e0
LucaKamps <111793@st-maartenscollege.nl>
2025-09-15 07:44:01 +0000

    Opdracht 13

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var xJOS = 350;
-var yJOS = 350;
+var xJOS;
+var yJOS;
 
 function setup() {
   canvas = createCanvas(450,450);
@@ -11,13 +11,18 @@ function setup() {
 function draw() {
   background('lavender');
   fill('black');
-  text("mouseX:" + round(mouseX) + " mouseY:" + round(mouseY),10,20);
-  text("xJOS:" + xJOS + " yJOS:" + yJOS,300,20);
-  translate(mouseX, mouseY);
+  text("xJOS: " + round(xJOS) + " (mouseX:" + round(mouseX) + ")",10,20);
+  text("yJOS: " + round(yJOS) + " (mouseY:" + round(mouseY) + ")",260,20);
+
+  xJOS = constrain(mouseX,75,375);
+  yJOS = constrain(mouseY,75,375);
+
+  translate(xJOS,yJOS);
 
   // in de volgende regels wordt JOS getekend
 
   push();
+  scale(0.5);
   noStroke();
   fill('indianred');
   ellipse(0,0,150);
@@ -34,8 +39,6 @@ function draw() {
   fill('white');
   arc(0, 40, 80, 40, 0, PI, CHORD);
   pop();
-  xJOS--;
-  yJOS-=2;
   // einde tekenen JOS
 
 }
