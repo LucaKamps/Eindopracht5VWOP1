eae61d5e994474cc76acfbe283024601cc2d858f
LucaKamps <111793@st-maartenscollege.nl>
2025-09-15 06:52:59 +0000

    Opdracht 9

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
+var letterGrootte = 30;
+var marge = 20;
+
 function setup() {
-  canvas = createCanvas(900,200);
-  background('lavender');
+  canvas = createCanvas(1000,300);
   canvas.parent();
-  noLoop();
+  textFont("Josefin Sans");
+  textSize(letterGrootte);
+  textLeading(1.9*letterGrootte);
+  //noLoop();
 }
 
 function draw() {
-  noStroke();
-  fill('thistle');
+  background('orange');
+  fill('yellow');
+  textAlign(LEFT, BOTTOM);
+  text("mouseX:"+round(mouseX) + " mouseY:"+round(mouseY),mouseX,mouseY);
   translate(50,50);
-  rect(0,0,100,100);
+  fill('black');
+  text("vensterGrootte:\n" + windowWidth + " x " + windowHeight,0,0);
+  text("canvasGrootte:\n " + width + " x " + height,0,120);
 
-  translate(140,0);
-  rect(0,0,100,100);
-  translate(140,0);
-  rect(0,0,100,100);
+  translate(500,0);
+  fill('dodgerblue');
+  stroke('white');
+  strokeWeight(10);
+  rect(0,0,400,200);
 
-  translate(140,0);
-  // BEGIN aanpassing
-  push();
-  stroke('cornsilk');
-  strokeWeight(20);
-  fill('teal');
-  translate(0,40);  
-  rect(0,0,100,100); 
-  // EINDE aanpassing
-
-  translate(140,0);
-  rect(0,0,100,100);
-  pop();
-  translate(140,0);
-  rect(0,0,100,100);  
+  translate(25,25);
+  noStroke();
+  fill('white');
+  textAlign(CENTER, CENTER);
+  text("Deze tekst is erg lang en wordt daarom op meerdere regels weergegeven.",0,0,350,150);
 }
