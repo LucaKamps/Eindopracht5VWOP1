a38b406c8af5536d566107b34adf87aeefbed4f2
LucaKamps <111793@st-maartenscollege.nl>
2025-09-22 07:44:34 +0000

    Opdracht 30

--- script.js
+++ script.js
@@ -1,54 +1,24 @@
-  var aantal = 4;
-  var breedte;
+var rij = 0;
+var kolom = 0;
 
-  function setup() {
-    canvas = createCanvas(1000,300);
-    canvas.parent();
-    noStroke();
-    textFont("Verdana");
-    textSize(14);
-    frameRate(5);
-  }
+function setup() {
+  canvas = createCanvas(451,451);
+  canvas.parent();
+  background('coral');
+  fill('white');
+}
 
-  function draw() {
-    background('lavender');
-    fill('black');
-    breedte = width/aantal;
-    text("aantal = " + aantal,10,20);  
-    push();
-    translate(0.5*breedte,150);
-
-    for (var n = 0;n < aantal;n++) {
-      tekenJos(2 + n / 2);
-      translate(breedte,0);
+function draw() {
+  for (var rij = 0;rij < 450;rij += 50) {
+    for (var kolom = 0;kolom < 450;kolom += 50) {
+      if (rij == 150 && kolom == 300) {
+        fill('orange');
+      }
+      else {
+        fill('white');
+      }
+      rect(kolom,rij,50,50);
     }
-    pop();
-
-    if (keyIsDown(LEFT_ARROW) && aantal >= 2) {
-      aantal--;
-    }
-    if (keyIsDown(RIGHT_ARROW)) {
-      aantal++;
-    }   
   }
-
-  function tekenJos(s) {
-    push();
-    scale(s);
-    noStroke();
-    fill('indianred');
-    ellipse(0,0,50);
-    fill('slategray');
-    ellipse(-7,-10,17);
-    ellipse(7,-10,17);
-    fill('white');
-    ellipse(-7,-8,7,13);
-    ellipse(7,-8,7,13);
-    fill('orange');
-    ellipse(0,3,17);
-    stroke('slategray');
-    strokeWeight(3);
-    fill('white');
-    arc(0, 13, 26, 13, 0, PI, CHORD);
-    pop();
-  }
+  
+}
