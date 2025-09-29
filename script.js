b34c1408686b15d306fe27f226d8ef9cfb18cc9e
LucaKamps <111793@st-maartenscollege.nl>
2025-09-29 07:51:36 +0000

    Opdracht 20

--- script.js
+++ script.js
@@ -1,40 +1,78 @@
-var bomen;
+var raster = {
+  aantalRijen: 6,
+  aantalKolommen: 9,
+  celGrootte: null,
 
-var kever = {
-  x: 100,
-  y: 150,
-  sprite: null,
-  naam : "Bart Brink",
+  berekenCelGrootte() {
+    this.celGrootte = width / this.aantalKolommen;
+  },
+  teken() {
+    push();
+    noFill();
+    stroke('grey');
+    for (var rij = 0;rij < this.aantalRijen;rij++) {
+      for (var kolom = 0;kolom < this.aantalKolommen;kolom++) {
+        rect(kolom*this.celGrootte,rij*this.celGrootte,this.celGrootte,this.celGrootte);
+      }
+    }
+    pop();
+  }
+}
+
+var jos = {
+  x: 400,
+  y: 300,
+  animatie: [],
+  aantalFrames: 6,
+  frameNummer: 3,
+  stapGrootte: null,
 
   beweeg() {
-    this.x += round(random(-5,5));
-    this.y += round(random(-5,5));
-    image(this.sprite,kever.x,kever.y,this.sprite.width / 2,this.sprite.height / 2);
+    if (keyIsDown(LEFT_ARROW)) {
+      this.x -= this.stapGrootte;
+      this.frameNummer = 2;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+      this.x += this.stapGrootte;
+      this.frameNummer = 1;
+    }
+    if (keyIsDown(UP_ARROW)) {
+      this.y -= this.stapGrootte;
+      this.frameNummer = 4;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+      this.y += this.stapGrootte;
+      this.frameNummer = 5;
+    }
+
+    this.x = constrain(this.x,0,width - raster.celGrootte);
+    this.y = constrain(this.y,0,height - raster.celGrootte);
+  },
+
+  toon() {
+    image(this.animatie[this.frameNummer],this.x,this.y,raster.celGrootte,raster.celGrootte);
   }
-};
+}
 
 function preload() {
-  bomen = loadImage("images/bomen.jpg");
-  kever.sprite = loadImage("images/sprites/kever.png");
+  brug = loadImage("images/backgrounds/dame_op_brug_1800.jpg");
+  for (var b = 0;b < jos.aantalFrames;b++) {
+    frame = loadImage("images/sprites/Jos100px/Jos_" + b + ".png");
+    jos.animatie.push(frame);
+  }
 }
 
 function setup() {
-  canvas = createCanvas(450,450);
+  canvas = createCanvas(900,600);
   canvas.parent();
-  textFont("Verdana");
-  textSize(14);
-  noStroke();
-  frameRate(2);
+  frameRate(10);
+  raster.berekenCelGrootte();
+  jos.stapGrootte = 2*raster.celGrootte;
 }
 
 function draw() {
-  background(bomen);
-
-  kever.beweeg();
-
-
-  fill('cornsilk');
-  rect(0,410,450,40);
-  fill('black');
-  text(kever.naam+" bevindt zich in het punt x = " + kever.x + " en y = " + kever.y + ".",5,435);
- }
+  background(brug);
+  raster.teken();
+  jos.beweeg();
+  jos.toon();
+}
