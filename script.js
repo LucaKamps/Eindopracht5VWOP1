4bd978615a6df4dbe4b3c35695870b22468c40d7
LucaKamps <111793@st-maartenscollege.nl>
2025-10-09 12:56:47 +0000

    Dag 1

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
+// hier wordt de klasse Raster gedefinieerd
 class Raster {
   constructor(r,k) {
-    this.aantalRijen = r;
-    this.aantalKolommen = k;
+    this.aantalRijen = 12;
+    this.aantalKolommen = 18;
     this.celGrootte = null;
   }
+
+  // bereken de grootte van een cel
 
   berekenCelGrootte() {
     this.celGrootte = canvas.width / this.aantalKolommen;
   }
 
+  // teken het raster
   teken() {
     push();
     noFill();
@@ -22,6 +26,7 @@ class Raster {
   }
 }
 
+// hier wordt de klasse Jos gedefinieerd
 class Jos {
   constructor() {
     this.x = 400;
@@ -31,21 +36,22 @@ class Jos {
     this.stapGrootte = null;
     this.gehaald = false;
   }
+  // beweeg Jos met de pijltjestoetsen
 
   beweeg() {
-    if (keyIsDown(LEFT_ARROW)) {
+    if (keyIsDown(65) === true) {
       this.x -= this.stapGrootte;
       this.frameNummer = 2;
     }
-    if (keyIsDown(RIGHT_ARROW)) {
+    if (keyIsDown(68) === true) {
       this.x += this.stapGrootte;
       this.frameNummer = 1;
     }
-    if (keyIsDown(UP_ARROW)) {
+    if (keyIsDown(87) === true) {
       this.y -= this.stapGrootte;
       this.frameNummer = 4;
     }
-    if (keyIsDown(DOWN_ARROW)) {
+    if (keyIsDown(83) === true) {
       this.y += this.stapGrootte;
       this.frameNummer = 5;
     }
@@ -57,6 +63,7 @@ class Jos {
       this.gehaald = true;
     }
   }
+  // controleer of Jos geraakt wordt door een vijand
 
   wordtGeraakt(vijand) {
     if (this.x == vijand.x && this.y == vijand.y) {
@@ -72,6 +79,8 @@ class Jos {
   }
 }  
 
+// hier wordt de klasse Vijand gedefinieerd
+
 class Vijand {
   constructor(x,y) {
     this.x = x;
@@ -79,6 +88,8 @@ class Vijand {
     this.sprite = null;
     this.stapGrootte = null;
   }
+
+  // beweeg de vijand willekeurig
 
   beweeg() {
     this.x += floor(random(-1,2))*this.stapGrootte;
@@ -97,6 +108,7 @@ function preload() {
   brug = loadImage("images/backgrounds/dame_op_brug_1800.jpg");
 }
 
+// hier worden de objecten gedefinieerd
 function setup() {
   canvas = createCanvas(900,600);
   canvas.parent();
@@ -115,6 +127,8 @@ function setup() {
     eve.animatie.push(frameEve);
   }
 
+  // Extra vijanden Alice en Bob
+
   alice = new Vijand(700,200);
   alice.stapGrootte = 1*eve.stapGrootte;
   alice.sprite = loadImage("images/sprites/Alice100px/Alice.png");
@@ -122,7 +136,14 @@ function setup() {
   bob = new Vijand(600,400);
   bob.stapGrootte = 1*eve.stapGrootte;
   bob.sprite = loadImage("images/sprites/Bob100px/Bob.png");  
+
+  // Extra vijand Cindy
+  cindy = new Vijand(500,100);
+  cindy.stapGrootte = 1*eve.stapGrootte;
+  cindy.sprite = loadImage("images/sprites/Alice100px/Alice.png");
 }
+
+// hier wordt het spel getekend
 
 function draw() {
   background(brug);
@@ -130,18 +151,26 @@ function draw() {
   eve.beweeg();
   alice.beweeg();
   bob.beweeg();
+  cindy.beweeg();
   eve.toon();
   alice.toon();
   bob.toon();
+  cindy.toon();
 
-  if (eve.wordtGeraakt(alice) || eve.wordtGeraakt(bob)) {
+  if (eve.wordtGeraakt(alice) || eve.wordtGeraakt(bob) || eve.wordtGeraakt(cindy)){
     noLoop();
   }
+  // geeft text als je gewonnen hebt
 
   if (eve.gehaald) {
     background('green');
     fill('white');
     text("Je hebt gewonnen!",30,300);
     noLoop();
+  }
+  if (eve.nietGehaald) {
+    background('red');
+    fill('white');
+    text("Je hebt verloren!",30,300);
   }
 }
