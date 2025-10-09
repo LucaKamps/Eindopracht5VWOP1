12ee2a1a8322d8e078cf2b0d3460847b96f40723
LucaKamps <111793@st-maartenscollege.nl>
2025-10-09 12:18:46 +0000

    Standaard versie

--- script.js
+++ script.js
@@ -1,56 +1,147 @@
-var aantalLagen = 5;
-var breedte = 90;
-var hoogte;
+class Raster {
+  constructor(r,k) {
+    this.aantalRijen = r;
+    this.aantalKolommen = k;
+    this.celGrootte = null;
+  }
+
+  berekenCelGrootte() {
+    this.celGrootte = canvas.width / this.aantalKolommen;
+  }
+
+  teken() {
+    push();
+    noFill();
+    stroke('grey');
+    for (var rij = 0;rij < this.aantalRijen;rij++) {
+      for (var kolom = 0;kolom < this.aantalKolommen;kolom++) {
+        rect(kolom*this.celGrootte,rij*this.celGrootte,this.celGrootte,this.celGrootte);
+      }
+    }
+    pop();
+  }
+}
+
+class Jos {
+  constructor() {
+    this.x = 400;
+    this.y = 300;
+    this.animatie = [];
+    this.frameNummer =  3;
+    this.stapGrootte = null;
+    this.gehaald = false;
+  }
+
+  beweeg() {
+    if (keyIsDown(LEFT_ARROW)) {
+      this.x -= this.stapGrootte;
+      this.frameNummer = 2;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+      this.x += this.stapGrootte;
+      this.frameNummer = 1;
+    }
+    if (keyIsDown(UP_ARROW)) {
+      this.y -= this.stapGrootte;
+      this.frameNummer = 4;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+      this.y += this.stapGrootte;
+      this.frameNummer = 5;
+    }
+
+    this.x = constrain(this.x,0,canvas.width);
+    this.y = constrain(this.y,0,canvas.height - raster.celGrootte);
+
+    if (this.x == canvas.width) {
+      this.gehaald = true;
+    }
+  }
+
+  wordtGeraakt(vijand) {
+    if (this.x == vijand.x && this.y == vijand.y) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  toon() {
+    image(this.animatie[this.frameNummer],this.x,this.y,raster.celGrootte,raster.celGrootte);
+  }
+}  
+
+class Vijand {
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = null;
+    this.stapGrootte = null;
+  }
+
+  beweeg() {
+    this.x += floor(random(-1,2))*this.stapGrootte;
+    this.y += floor(random(-1,2))*this.stapGrootte;
+
+    this.x = constrain(this.x,0,canvas.width - raster.celGrootte);
+    this.y = constrain(this.y,0,canvas.height - raster.celGrootte);
+  }
+
+  toon() {
+    image(this.sprite,this.x,this.y,raster.celGrootte,raster.celGrootte);
+  }
+}
+
+function preload() {
+  brug = loadImage("images/backgrounds/dame_op_brug_1800.jpg");
+}
 
 function setup() {
-  hoogte = breedte / 2;
-  canvas = createCanvas(800,600);
+  canvas = createCanvas(900,600);
   canvas.parent();
-  noLoop();
+  frameRate(10);
+  textFont("Verdana");
+  textSize(90);
+
+  raster = new Raster(6,9);
+
+  raster.berekenCelGrootte();
+
+  eve = new Jos();
+  eve.stapGrootte = 1*raster.celGrootte;
+  for (var b = 0;b < 6;b++) {
+    frameEve = loadImage("images/sprites/Eve100px/Eve_" + b + ".png");
+    eve.animatie.push(frameEve);
+  }
+
+  alice = new Vijand(700,200);
+  alice.stapGrootte = 1*eve.stapGrootte;
+  alice.sprite = loadImage("images/sprites/Alice100px/Alice.png");
+
+  bob = new Vijand(600,400);
+  bob.stapGrootte = 1*eve.stapGrootte;
+  bob.sprite = loadImage("images/sprites/Bob100px/Bob.png");  
 }
 
 function draw() {
-    tekenKamer(1);
-}
+  background(brug);
+  raster.teken();
+  eve.beweeg();
+  alice.beweeg();
+  bob.beweeg();
+  eve.toon();
+  alice.toon();
+  bob.toon();
 
-function tekenKamer(s) {
-    scale(s);
-    fill('red');
-    noStroke();
-    rect(0,0,800,600);
-    fill('blue');
-    rect(0,0,400,600);
-    stroke(1);
-    fill('mediumseagreen');
-    rect(50,200,250,400);
-    fill('lightyellow');
-    rect(70,220,210,160);
-    noStroke();
-    fill('brown');
-    ellipse(600,525,150);
-    translate(170,100);
-    tekenBloem();
-    translate(-170,-100);
-    fill('black');
-    rect(340,90,420,320);
+  if (eve.wordtGeraakt(alice) || eve.wordtGeraakt(bob)) {
+    noLoop();
+  }
 
-    translate(350,100);
-    tekenKamer(0.5);
-}
-
-function tekenBloem() {
-    var aantal = 25;
-     push();
-     colorMode(RGB, 255, 255, 255, 1);
-     fill(178, 34, 34,.7);
-     for (var n = 0;n < aantal;n++) {
-         ellipse(0,0,150,20);
-         rotate(360 / aantal);
-     }
-     fill(255, 195, 0,.5);
-     for (var n = 0;n < aantal;n++) {
-         rect(0,0,35,35)
-         rotate(360 / aantal);
-     }
-     pop();
+  if (eve.gehaald) {
+    background('green');
+    fill('white');
+    text("Je hebt gewonnen!",30,300);
+    noLoop();
+  }
 }
